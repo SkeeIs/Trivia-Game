5c34f3d2350ddfb4879626cc331fbc770bccff41
Taylor Skeels <skeels@Taylors-MacBook-Pro.local>
2018-08-09 17:51:16 -0700

    logic fully functional. js needs to DRY out, styling needs to be applied

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,108 +3,130 @@
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
-var timeCount = 15;
+var timeCount;
 var timer;
-var timesUp;
 var index = 0;
 $("#question").hide();
 $("#answers").hide();
 $("#results").hide();
 
-//stop countdown function that clears interval
-function stopCountdown() {
-    clearInterval(timer);
-    unanswered++;
-    //add timer for below text to display for 3 sec before changeQuestion is called
-    
-    timer = setInterval(function() {
-        timeCount --;
-        if (timeCount === 0) {
-        //    timesUp = setInterval(function() {
-        //         $("#question-container").val("Time's Up! Get ready for the next question!");
-        //     }, 3000);
-            changeQuestion();
-            if (index  > allQuestions.length) {
-                clearInterval(timer);
-                endGame();
-            }  
-        }
-    }, 1000);
-}
-
 //reset function that resets all variables, "start screen"
 function reset() {
-    correct = 0;
-    incorrect = 0;
-    $("#start-button").show();
+  correct = 0;
+  incorrect = 0;
+  unanswered = 0;
+  index = 0;
+  $("#start-button").show();
 }
 //changeQuestion function that condition checks there are questions left in our object, changes html, calls start countdown
 function changeQuestion() {
-    startCountdown();
-    $("#question").empty();
-    $("#answers").empty();
-    $("#question").html(allQuestions[index].q);
-    for (var i = 0; i < allQuestions[index].p.length; i++) {
-      $("#answers").append($("<div>")).text(allQuestions[index].p[i]);
-      console.log(allQuestions[index].p[i]);
-      //newAnswerDiv.attr("data-name", allQuestions[index].p[i]);
-      //newAnswerDiv.text(allQuestions[index].p[i]);
-      //$("#answers").append(newAnswerDiv);
-      //console.log(newAnswerDiv);
+  startCountdown();
+  $("#question").empty();
+  $("#answers").empty();
+  $("#question").html(allQuestions[index].q);
+  
+  for (var i = 0; i < allQuestions[index].p.length; i++) {
+      var newAnswerDiv = $("<div>");
+      newAnswerDiv.attr("data-name", allQuestions[index].p[i]);
+      newAnswerDiv.attr("class", "possible-answer");
+      newAnswerDiv.text(allQuestions[index].p[i]);
+      $("#answers").append(newAnswerDiv);
+      console.log(newAnswerDiv);
     }
-    index++;
-
+    
 }
-//endGame function that calculates questions answered correctly vs incorrectly vs unanswered & returns to html for x amount of time, then changes to start button for replayability
+//endGame function that shows user their results
 function endGame() {
-    stopCountdown();
-    $("#question").empty();
-    $("#answers").empty();
-    $("#results").show();
-    $("#results").append($("<p>").text("Correct: " + correct));
-    $("#results").append($("<p>").text("Incorrect: " + incorrect));
-    $("#results").append($("<p>").text("Unanswered: " + unanswered));
+  clearInterval(timer);
+  $("#question").hide();
+  $("#answers").hide();
+  $("#time-left").hide();
+  $("#question-container").empty();
+  $("#results").show();
+  $("#results").html($("<p>").text("Correct: " + correct));
+  $("#results").append($("<p>").text("Incorrect: " + incorrect));
+  $("#results").append($("<p>").text("Unanswered: " + unanswered));
+    reset();
 }
 
 //start countdown function that clears interval, sets timeCount = timePerQuestion, calls decrement function, output to time-left div
 function startCountdown() {
-    timeCount = 3;
+    timeCount = 15;
     $("#time-left").html("Time Left: " + timeCount);
     timer = setInterval(function() {
         timeCount --;
         $("#time-left").html("Time Left: " + timeCount);
+        
         if (timeCount === 0) {
-            stopCountdown();
-            changeQuestion();
-            incorrect ++;
-            console.log(index);
-            console.log(allQuestions.length);
-            if (index  > allQuestions.length) {
-                clearInterval(timer);
+            
+            if (index  >= allQuestions.length - 1) {
+                console.log(index);
+                //clearInterval(timer);
                 endGame();
             }  
+            
+            else {
+                clearInterval(timer);
+                unanswered++;
+                $("#question").empty();
+                $("#answers").empty();
+                $("#question-container").text("Time's Up! The correct answer was " + allQuestions[index].a);
+                index++;
+                setTimeout(function() {
+                    $("#question-container").empty();
+                    changeQuestion();
+                    
+                }, 3000);
+            }      
         }
     }, 1000);
 }
 
 //on click of start button "start screen" gone & first question + timer displays instead
 $("#start-button").on("click", function() {
-    changeQuestion();
     $("#start-button").hide();
+    $("#results").hide();
+    $("#question-container").empty();
     $("#question").show();
     $("#answers").show();
+    $("#time-left").show();
+    changeQuestion();
 })
 //on click of answer, stores user input, calls changeQuestion function
-// $("#answers").on("click", function() {
-//     var userPick = value from selected answer;
+ $("#answers").on("click", ".possible-answer", function() {
+    console.log(index); 
+    var userPick = $(this).attr("data-name");
+        console.log(userPick);
+        if (userPick === allQuestions[index].a) {
+           correct++;
+           index++;
+           $("#question").empty();
+           $("#answers").empty();
+           $("#question-container").text("Out of this world!");
+        }
 
-        //if (userPick === allQuestions[index].a) {
-          //correct++;
-          //$("#question-container").val("CORRECT!");
-        //}
+        else {
+            $("#question-container").text("INCORRECT! The correct answer was: " + allQuestions[index].a);
+            incorrect++;
+            index++;
+        }
 
-        //else if (userPick !== allQuestions[index].a) {
-            //incorrect++;
-            //$("#question-container").val("INCORRECT! The correct answer was: " + allQuestions[index].a);
-        //}
-// })
+        if (index  > allQuestions.length - 1) {
+            clearInterval(timer);
+            endGame();
+        }  
+        
+        else {
+            clearInterval(timer);
+            $("#question").empty();
+            $("#answers").empty();
+            //$("#question-container").text("Time's Up! The correct answer was " + allQuestions[index].a);
+            
+            setTimeout(function() {
+                $("#question-container").empty();
+                changeQuestion();
+   
+            }, 3000);
+        
+        }   
+})
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,7 +1,7 @@
 var allQuestions = [
     {
         q:"Which statement best describes galaxies?", 
-        a:"They may contain billions of stars in a  space much larger than our solar system.", 
+        a:"They may contain billions of stars in a space much larger than our solar system", 
         p:["They are similar in size to a solar system", "They may contain billions of stars in a space much larger than our solar system", "They contain only one star but hundreds of planets.", "They may contain a few hundred stars in a space slightly larger than the solar system."]
     },
 
@@ -17,43 +17,43 @@ var allQuestions = [
         p:["concentration in the yellow portion of the spectrum","concentration in the green portion of the spectrum","shift toward the blue end of the spectrum","shift toward the red end of the spectrum"]
     },
 
-    // {
-    //     q:"In which list are celestial features correctly shown in order of increasing size?", 
-    //     a:"planet -- solar system -- galaxy -- universe", 
-    //     p:["planet -- solar system -- galaxy -- universe","galaxy -- solar system -- universe -- planet","solar system -- galaxy -- planet -- universe","universe -- galaxy -- solar system -- planet"]
-    // },
+    {
+        q:"In which list are celestial features correctly shown in order of increasing size?", 
+        a:"planet -- solar system -- galaxy -- universe", 
+        p:["planet -- solar system -- galaxy -- universe","galaxy -- solar system -- universe -- planet","solar system -- galaxy -- planet -- universe","universe -- galaxy -- solar system -- planet"]
+    },
 
-    // {
-    //     q:"Based on the red-shift data on galaxies, most astronomers infer that the universe is currently", 
-    //     a:"expanding", 
-    //     p:["moving randomly","contracting","expanding","fixed and stationary"]
-    // },
+    {
+        q:"Based on the red-shift data on galaxies, most astronomers infer that the universe is currently", 
+        a:"expanding", 
+        p:["moving randomly","contracting","expanding","fixed and stationary"]
+    },
 
-    // {
-    //     q:"The unit most used by astronomers to express the distances to other galaxies is", 
-    //     a:"Lightyears (LY)", 
-    //     p:["Lightyears (LY)","astronomical unit (AU)","miles","kilometers"]
-    // },
+    {
+        q:"The unit most used by astronomers to express the distances to other galaxies is", 
+        a:"Lightyears (LY)", 
+        p:["Lightyears (LY)","astronomical unit (AU)","miles","kilometers"]
+    },
 
-    // {
-    //     q:"A light year is", 
-    //     a:"the distance traveled by light in one year", 
-    //     p:["the time it takes light to go once around the Earth's orbit","the time it takes light to travel one year","the distance the Earth moves in one year","the distance traveled by light in one year"]
-    // },
-    // {
-    //     q:"The Milky Way galaxy is best described as", 
-    //     a:"a spiral-shaped formation composed of billions of stars", 
-    //     p:["a spiral-shaped formation composed of billions of stars","a constellation visible to everyone on Earth","a region in space between the orbits of Mars and Jupiter","a type of solar system"]
-    // },
-    // {
-    //     q:"Planets, stars & galaxies that we can see make up only a part of the universe. What percent of the universe is currently undetectable to us?", 
-    //     a:"96%", 
-    //     p:["40%","8%","96%","68%"]
-    // },
-    // {
-    //     q:"According to Hubbell's law more rapidly moving galaxies are now", 
-    //     a:"further away from us", 
-    //     p:["closer to us","accelerating rapidly","already contracting toward another big bang","further away from us"]
-    // }
+    {
+        q:"A light year is", 
+        a:"the distance traveled by light in one year", 
+        p:["the time it takes light to go once around the Earth's orbit","the time it takes light to travel one year","the distance the Earth moves in one year","the distance traveled by light in one year"]
+    },
+    {
+        q:"The Milky Way galaxy is best described as", 
+        a:"a spiral-shaped formation composed of billions of stars", 
+        p:["a spiral-shaped formation composed of billions of stars","a constellation visible to everyone on Earth","a region in space between the orbits of Mars and Jupiter","a type of solar system"]
+    },
+    {
+        q:"Planets, stars & galaxies that we can see make up only a part of the universe. What percent of the universe is currently undetectable to us?", 
+        a:"96%", 
+        p:["40%","8%","96%","68%"]
+    },
+    {
+        q:"According to Hubbell's law more rapidly moving galaxies are now", 
+        a:"further away from us", 
+        p:["closer to us","accelerating rapidly","already contracting toward another big bang","further away from us"]
+    }
   ]
 
